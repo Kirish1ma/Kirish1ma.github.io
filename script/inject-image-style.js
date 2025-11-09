6017ae79809dff53084710fee8eae4852a2a922c
Kirish1ma <0rangethree63@gmail>
2025-11-10 04:02:54 +0800

    Site updated: 2025-11-10 04:02:54

--- script/inject-image-style.js
+++
@@ -1,9 +0,0 @@
-hexo.extend.injector.register(
-  "head_end",
-  () => {
-    return css(
-      "<link rel='stylesheet' href='Hexo-blog/source/script/css/inject-image-style.html'>"
-    );
-  },
-  "post"
-);
